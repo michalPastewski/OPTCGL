e25d767e32114b5131f92d924398da8b07b18580
MPastewski <michpastewski@gmail.com>
2025-02-07 08:42:21 +0100

    Initial commit.
    Add basic configuration from Create Next App with shadcnUI.
    Add some custom colors.

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'One Piece Trading Card Gama League',
+  description:
+    'Bring competetive gaming to the next level and for Players to be able to Play against the best in the whole country.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
